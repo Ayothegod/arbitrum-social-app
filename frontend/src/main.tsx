be246d8b369e9c923c6c02cffeb8414e5919a21a
Ayothegod <heyayomideadebisi@gmail.com>
2024-10-31 12:25:30 +0100

    base index file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,13 @@
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import "@rainbow-me/rainbowkit/styles.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { WagmiProvider } from "wagmi";
 import App from "./App.tsx";
-import "./index.css";
 import { config } from "./wagmi.ts";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,11 +1,45 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Link, useRouteError } from "react-router-dom";
 
+import styles from "../styles/Home.module.css";
+import { useAccount } from "wagmi";
+
 export default function Index() {
   return (
-    <div>
-      Index
-      <ConnectButton />
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <span className={styles.stackup}>StackUp</span> x{" "}
+          <span className={styles.arbitrum}>Arbitrum</span> Forums
+        </h1>
+        <ConnectButton label="Sign In" />
+        <br />
+        <p className={styles.description}>
+          {useAccount().isConnected && (
+            <>
+              You are connected. Head to our{" "}
+              <Link className={styles.glitter} to="/forum">
+                Forum
+              </Link>{" "}
+              now!
+            </>
+          )}
+        </p>
+      </main>
+
+      <footer className={styles.footer}>
+        <p>
+          Step into Arbitrum campaign, brought to you by{" "}
+          <a href="https://stackup.dev" target="_blank" rel="noreferrer">
+            StackUp
+          </a>{" "}
+          ❤️ in partnership with{" "}
+          <a href="https://arbitrum.io" rel="noreferrer" target="_blank">
+            Arbitrum!
+          </a>{" "}
+          💙🧡
+        </p>
+      </footer>
     </div>
   );
 }
