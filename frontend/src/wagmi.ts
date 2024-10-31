e2430b77998f087bab0117f06d55396b86a9cd66
Ayothegod <heyayomideadebisi@gmail.com>
2024-10-31 10:18:42 +0100

    configure wagmi

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -1,10 +1,27 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { arbitrum, arbitrumSepolia, mainnet } from "wagmi/chains";
+import {
+  arbitrum,
+  arbitrumSepolia,
+  mainnet,
+  anvil,
+  hardhat,
+} from "wagmi/chains";
+
+const projectId = "37995902ad353a1dfa24c6f0f1aea540" as string;
 
 export const config = getDefaultConfig({
   appName: "Arbitrum Posting App",
-  projectId: "37995902ad353a1dfa24c6f0f1aea540",
-  chains: [mainnet, arbitrum, arbitrumSepolia],
+  projectId: projectId,
+  chains:
+    process.env.NEXT_PUBLIC_ONLY_ANVIL_HARDHAT_TESTNETS === "true"
+      ? [anvil, hardhat]
+      : [
+          mainnet,
+          arbitrum,
+          ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true"
+            ? [arbitrumSepolia, anvil]
+            : []),
+        ],
   ssr: false,
 });
 
